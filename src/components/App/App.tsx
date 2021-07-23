9495ed694ee8514e2b0ee1d69b89c8da8af2aa40
karolina-michalak <karolina0michalak@gmail.com>
2021-07-23 16:54:56 +0200

    Changes

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Recorder from "../Recorder/Recorder";
+
+function App() {
+  return (
+    <div className="App">
+      <Recorder />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/user-events.ts
@@ -0,0 +1,28 @@
+interface UserEvent {
+  id: number;
+  title: string;
+  dateStart: string;
+  dateEnd: string;
+}
+
+interface UserEventsState {
+  byIds: Record<UserEvent["id"], UserEvent>;
+  allIds: UserEvent["id"][];
+}
+
+const initialState: UserEventsState = {
+  byIds: {},
+  allIds: [],
+};
+
+const userEventsReducer = (
+  state: UserEventsState = initialState,
+  action: any
+) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userEventsReducer;
